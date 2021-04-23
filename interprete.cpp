93527d302b4c83b984c6380bfed0d8d5bbdd2276
Rodrigo Castro <rcc.dark@gmail.com>
2021-04-22 21:05:46 -0500

    fix main (argv)
    y ejemplo más complicado

--- interprete.cpp
+++ interprete.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include "lib/lexer.h"
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
@@ -11,11 +12,12 @@ extern "C" {
 EMSCRIPTEN_KEEPALIVE
 #endif
 const char* interpreta(const char* codigo) {
+   std::cerr << codigo << "\n";
    static std::string res = "";
    //something
    std::vector<token_anotada> v = lexer(codigo);
    for(int i = 0; i < v.size( ); ++i){
-      std::cout << v[i].location << '\n';
+      std::cout << v[i].tipo << " " << v[i].location << '\n';
    }
    return res.c_str( );
 }
@@ -25,23 +27,19 @@ int main(int argc, const char* argv[]) {
 #ifdef __EMSCRIPTEN__
    return 0;
 #else
-   /*
    if(argc < 2){
       std::cout << "Modo de uso: " << argv[0] << " ruta_codigo";
       return 0;
-   }*/
-   std::string s;
-   std::ifstream file;
-   file.open("cpp.txt", std::ios::in);
-   std::string r;
-   if(file.is_open( )) {
-      std::cout << "leyendo..." << std::endl;
-      while (!file.eof()) {
-         std::getline(file, r);
-         s+=r;
-      }
    }
-   std::cout << interpreta(s.c_str());
-   file.close( );
+
+   std::ifstream file(argv[1], std::ios::in);
+   if (!file.is_open( )) {
+      std::cout << "No se pudo abrir " << argv[1] << "\n";
+      return 0;
+   }
+
+   std::ostringstream buffer;
+   buffer << file.rdbuf( );
+   std::cout << interpreta(std::move(buffer).str( ).c_str( ));
 #endif
 }
